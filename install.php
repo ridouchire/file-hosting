094f02da47b52b3d970b741fd0ad9e94e687cc57
ridouchire <ridouchire@gmail.com>
2018-06-11 22:47:33 +0400

    [#3] Added installation script

---
+++ install.php
@@ -0,0 +1,29 @@
+<?php
+
+require(__DIR__ . "/app/conf.php");
+
+echo "==============\n";
+echo "Installation " . APP_VERSION . "\n";
+echo "-------------\n";
+
+if (is_dir(__DIR__ . '/files') == true) {
+    echo "Directory '/files' already exists\n";
+} else {
+    echo "Create directory ./files \n";
+    try {
+        mkdir(__DIR__ . '/files');
+    } catch (Exception $e) {
+        echo "Cannot create directory /files!\n Check permission on read/write for ./" . __DIR__ . "\n";
+    }
+}
+echo "-------------\n";
+echo "Set permission for directory ./files \n";
+try {
+    chmod(__DIR__ . '/files', 0777);
+} catch (Exception $e) {
+    echo "Cannot set permission for directory /files \n";
+}
+
+echo "-------------\n";
+echo "Installation completed \n";
+echo "==============\n";
