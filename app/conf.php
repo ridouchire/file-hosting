cbf77bc1d59c8044b1088a040607eb212757c0ce
ridouchire <ridouchire@gmail.com>
2018-06-11 23:08:15 +0400

    Security fix

--- app/conf.php
+++ app/conf.php
@@ -1,9 +1,10 @@
 <?php
 
+define('KERNEL', true);
 define('DIR_UPLOAD', '../files/');
 define('ALLOWED_TYPES', ['gif', 'jpg', 'jpeg', 'png', 'jpe'], true);
 // UNIQUE, TEMP, FILENAME
 define('FILENAME_TYPE', 'UNIQUE');
 
-//ini_set('display_errors', true);
-//error_reporting(E_ALL);
+ini_set('display_errors', true);
+error_reporting(E_ALL);
