78c6448738455f97d60aee4e777c670da482979c
ridouchire <ridouchire@gmail.com>
2018-06-11 21:03:18 +0400

    [#26] Core: Generate filename function was changed.

--- app/tests/FuncTest.php
+++ app/tests/FuncTest.php
@@ -7,12 +7,6 @@ require(__DIR__ . "/../func.php");
 
 class FuncTest extends TestCase
 {
-
-    public function setUp()
-    {
-        return;
-    }
-
     public function fnCheckFileTypeDataProvider()
     {
         return array(
@@ -31,5 +25,33 @@ class FuncTest extends TestCase
     public function testFnCheckFileType($extension, $expected)
     {
         $this->assertEquals($expected, fn_check_filetype($extension));
+    }
+
+    public function fnGenerateFilenameDataProvider()
+    {
+        return array(
+            array(
+                'name#53.jpg',
+                'temp',
+                'jpg',
+                'FILENAME',
+                'name53.jpg',
+            ),
+            array(
+                'name',
+                'temp',
+                'png',
+                'TEMP',
+                'temp.png',
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider fnGenerateFilenameDataProvider
+     */
+    public function testFnGenerateFilename($name, $temp, $ext, $type = FILENAME_TYPE, $expected)
+    {
+        $this->assertEquals($expected, fn_generate_filename($name, $temp, $ext, $type));
     }
 }
