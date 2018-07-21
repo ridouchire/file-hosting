001c12b1e9d9e85cbe85daca7f13ffa8178a1920
ridouchire <ridouchire@gmail.com>
2018-07-21 23:38:32 +0400

    [#42] Core: Schema "menu" has been added.

--- app/schemas/menu.php
+++ app/schemas/menu.php
@@ -1,7 +1,20 @@
-return array(
-    'index.php' => array(
+<?php
+
+$schemas['menu'] =  array(
+    array(
+        'link' => 'index.php',
         'name' => 'Upload',
+    ),
+    array(
+        'link' => 'https://rleading.jimdo.com/',
+        'name' => 'Dominionil Rechitskiy',
         'target' => 'blank',
     ),
-    ''
-)
+    array(
+        'link' => 'https://github.com/ridouchire/image-hosting/issues',
+        'name' => 'Issue Tracker',
+        'target' => 'blank',
+    )
+);
+
+return $schemas;
