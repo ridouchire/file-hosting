b4416f605d14bbedbfa89ba0c4dc9c16be9b1bf1
ridouchire <ridouchire@gmail.com>
2018-10-10 12:43:45 +0400

    [~] Release: Bump app version.

--- app/conf.php
+++ app/conf.php
@@ -1,7 +1,6 @@
 <?php
 
-define('KERNEL', true);
-define('APP_VERSION', 'ImageHosting v.1.0.0');
+define('APP_VERSION', 'ImageHosting v.1.0.2');
 define('DIR_UPLOAD', '../files/');
 define('ALLOWED_TYPES', ['gif', 'jpg', 'jpeg', 'png', 'jpe'], true);
 // UNIQUE, TEMP, FILENAME
