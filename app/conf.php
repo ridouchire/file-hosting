cd227f5e079e570456971c2cb614cb9044170fa0
ridouchire <ridouchire@gmail.com>
2018-10-16 23:29:25 +0400

    [~] {@5488} {@221} Release Builder: Tasks for deploy on staging and production was added.

--- app/conf.php
+++ app/conf.php
@@ -14,7 +14,7 @@ if (!defined('KERNEL')) {
     die("Access denied");
 }
 
-define('APP_VERSION', 'ImageHosting v.1.0.2');
+define('APP_VERSION', '1.0.3');
 define('DIR_UPLOAD', '../files/');
 define('ALLOWED_TYPES', ['gif', 'jpg', 'jpeg', 'png', 'jpe'], true);
 define('FILENAME_TYPE', 'UNIQUE');
@@ -23,8 +23,8 @@ define('UPLOAD_ERR_UNSUPPORTED', 9);
 define('TEST_SALT', '1010101');
 define('ALLOWED_TYPES', ['gif', 'jpg', 'jpeg', 'png', 'jpe'], true);
 
-if (is_file('../local_conf.php')) {
-    require '../local_conf.php';
+if (is_file('./local_conf.php')) {
+    require './local_conf.php';
 }
 
 if (defined('DEVELOPMENT')) {
