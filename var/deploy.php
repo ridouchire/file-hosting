523c95d1c25c754007ce9c75ef84f90131439fe3
ridouchire <ridouchire@gmail.com>
2018-10-25 19:19:01 +0400

    [~] {@5488} Release Builder: Task for deploying on production was added.

--- var/deploy.php
+++ var/deploy.php
@@ -12,47 +12,76 @@ set('default_stage', 'staging');
 host('demo.ume.69.mu')
     ->user('root')
     ->stage('staging')
-    ->set('stag_path', '/var/www/staging');
+    ->set('is_stag', 'Y')
+    ->set('web_path', '/var/www/staging');
 
 host('ume.69.mu')
     ->user('root')
     ->stage('prod')
-    ->set('prod_path', '/var/www/html');
+    ->set('is_stag', 'N')
+    ->set('web_path', '/var/www/html');
+
+function print_text($text) {
+    writeln("➤ $text");
+}
+
+function run_test()
+{
+    print_text("Running unit-tests...");
+    runLocally('../vendor/bin/phpunit  ../app/tests');
+}
+
+function copy_files()
+{
+    print_text("Removed and create directory 'release'");
+    runLocally('rm -rf release && mkdir release');
+    print_text("Copy app files to release directory");
+    runLocally('cp -R ../app ../css ../templates ../index.php ../install.php ./release');
+    print_text('copy local_conf.php in release archive');
+    runLocally('cp ../local_conf.php ./release');
+}
+
+function pack_release()
+{
+    print_text("Removed unit-tests dir and pack release to archive and removed temp files");
+    runLocally("cd release && rm -rf app/tests && tar -zcvf release.tar.gz ./* && rm -rf app/ css/ templates/ index.php install.php");
+}
 
 task('deploy:release', function() {
-    $stag = ask('Is staging deploying? [Y]', false);
-    run('../vendor/bin/phpunit  ../app/tests');
-    run('rm -rf release && mkdir release');
-    run('cp -R ../app ../css ../templates ../index.php ../install.php ./release');
-    if ($stag == 'Y') {
-        write('copy local_conf.php in release archive');
-        run('cp ../local_conf.php ./release');
-    }
-    run("cd release && rm -rf app/tests && tar -zcvf release.tar.gz ./* && rm -rf app/ css/ templates/ index.php install.php");
-})->local()->desc('Run unit-tests and build release archive');
+    run_test();
+    copy_files();
+    pack_release();
+})->desc('Run unit-tests and build release archive');
 
-task('deploy:update_code', function() {
+task('deploy:update_code', function() {;
     $release   = get('release');
     $code_path = get('code_path');
+    print_text("Moved to code dir");
     cd($code_path);
+    print_text("Uploaded release archive to code dir");
     upload("release/release.tar.gz", "$code_path/$release.tar.gz");
 })->desc("Upload release archive to remote host");
 
 task('deploy:restore', function() {
-    $stag_path = get('stag_path');
     $code_path = get('code_path');
     $release   = get('release');
-    cd($stag_path);
+    cd(get('web_path'));
+    print_text("Removed outdated app files in production dir");
     run("rm -rf ./*");
-    run("cp $code_path/$release.tar.gz ./ && tar xvf $release.tar.gz && rm $release.tar.gz && rm -rf files");
+    print_text("Copied release archive to production dir and unpacked and removed temp files");
+    run("cp $code_path/$release.tar.gz ./ && tar xvf $release.tar.gz && rm $release.tar.gz");
+    print_text("Run install script");
     run('php install.php && rm install.php');
+    if (get('is_stag') == 'N') {
+        print_text("Removed local_conf.php");
+        run("rm local_conf.php");
+    }
 })->desc("Unpack app archive and install app");
 
 task('deploy', function() {
-    run('/etc/init.d/nginx restart');
+    print_text("Restart php-fpm instance");
     run('/etc/init.d/php7.2-fpm restart');
 });
 
-before('deploy', 'deploy:release');
-after('deploy:release', 'deploy:update_code');
-after('deploy:update_code', 'deploy:restore');
+before('deploy', 'deploy:restore');
+before('deploy', 'deploy:update_code');
